77bd9393272b921584464366977cc031559cb070
minseo-hong <mshong1007@gmail.com>
2024-05-06 00:18:22 +0900

    [Init] 반응형 사이즈 그리드 시스템 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: '390px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px',
+      },
       colors: {
         primary: {
           DEFAULT: '#FAB00E',
