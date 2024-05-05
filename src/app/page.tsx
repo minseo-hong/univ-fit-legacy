bfb7723623667cf7729176c0ecf4b09c54830c9f
minseo-hong <mshong1007@gmail.com>
2024-05-06 00:59:35 +0900

    [Design] 검색 및 메뉴 아이콘 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import MenuIcon from '@/components/icons/MenuIcon';
+import SearchIcon from '@/components/icons/SearchIcon';
+
+const Home = () => {
   return (
     <main>
       <h1 className="title-lg font-semibold">안녕하세요.</h1>
+      <div className="flex gap-2 text-[1.5rem]">
+        <SearchIcon />
+        <MenuIcon />
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
