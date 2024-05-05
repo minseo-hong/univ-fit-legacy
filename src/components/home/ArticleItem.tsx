58785c3cedf68c237115433d7186388f1c1cdd3f
minseo-hong <mshong1007@gmail.com>
2024-05-06 02:00:53 +0900

    [Design] 홈페이지 제작

---
+++ src/components/home/ArticleItem.tsx
@@ -0,0 +1,23 @@
+interface ArticleItemProps {
+  as?: keyof JSX.IntrinsicElements;
+}
+
+const ArticleItem = ({ as: Wrapper = 'div' }: ArticleItemProps) => {
+  return (
+    <Wrapper>
+      <div className="flex w-[9.25rem] flex-col gap-2">
+        <div className="aspect-square w-full overflow-hidden rounded-xl">
+          <img
+            src="/images/placeholders/placeholder-image.png"
+            alt="아티클 이미지"
+          />
+        </div>
+        <h2 className="text-lg-200 w-full">
+          소득분위 산정 방법은 어떻게 이뤄질까?
+        </h2>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default ArticleItem;
