8fa9d3d9cda987a73ec43e616bca9289f4951e43
minseo-hong <mshong1007@gmail.com>
2024-05-20 22:12:32 +0900

    [Fix] 배포 오류 수정

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,10 +1,13 @@
 export const getPopularScholarshipList = async () => {
-  const res = await fetch(`${process.env.SERVER_API_URL}/home/announcements`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_API_URL}/home/announcements`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     },
-  });
+  );
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
