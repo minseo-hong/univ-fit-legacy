8d14fdd548abdbce625bd2b1f511fcf7f4ed202f
minseo-hong <mshong1007@gmail.com>
2024-05-22 21:52:28 +0900

    [Fix] 배포 오류 수정

--- src/app/auth/kakao/callback/page.tsx
+++ src/app/auth/kakao/callback/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import { fetchSocialLogin } from '@/api/auth';
 import { setTokenCookie } from '@/app/actions/cookies';
